2a46405b6e6e2726b41bb8d573f6cfcb21b11dd6
Kaspars Cinis <kaspars.cinis@gmail.com>
2018-09-20 17:00:33 +0200

    Updated footer

--- src/layouts/components/Footer/Footer.component.js
+++ src/layouts/components/Footer/Footer.component.js
@@ -12,22 +12,13 @@ function Footer({ ...props }) {
     return (
         <footer className={classes.footer}>
             <div className={classes.container}>
-                <div className={classes.left}>
-                    <List className={classes.list}>
-                        <ListItem className={classes.inlineBlock}>
-                            <a href="#home" className={classes.block}>
-                                Home
-                            </a>
-                        </ListItem>
-                    </List>
-                </div>
                 <p className={classes.right}>
-          <span>
-            &copy; {1900 + new Date().getYear()}{" "}
-              <a href="https://www.creative-tim.com" className={classes.a}>
-              OilfieldOS
-            </a>, made by best coder in this room
-          </span>
+                    <span>
+                        &copy; {1900 + new Date().getYear()}{" "}
+                        <a href="https://www.creative-tim.com" className={classes.a}>
+                            OilfieldOS
+                        </a>
+                    </span>
                 </p>
             </div>
         </footer>
